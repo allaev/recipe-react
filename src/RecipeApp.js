7c0203fd4095f382fd5b354f830a85e064640112
allaev <34710484+allaev@users.noreply.github.com>
2018-05-15 00:50:28 +0500

    add state

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -4,11 +4,41 @@ import RecipeList from './RecipeList';
 import './RecipeApp.css';
 
 class RecipeApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: [
+        {
+          id: 0,
+          title:"Pasta",
+          instructions:'Mix ingredients',
+          ingredients: ['Flour', 'Water'],
+          img:'spaghetti.jpg'
+        },
+        {
+          id: 1,
+          title: "Milkshake",
+          instructions: 'Combine ice cream and milk. Blend until creamy',
+          ingredients: ['2 Scoops Ice cream', '8 ounces milk'],
+          img: 'milkshake.jpg'
+        },
+        {
+          id: 2,
+          title: 'Avocado Toast',
+          instructions: 'Toast bread. Slice avocado and spread on bread. Add salt, oil, and pepper to taste.',
+          ingredients: ['2 slices of bread', '1 avocado', '1 tablespoon olive oil', '1 pinch salt', 'pepper'],
+          img: 'avocado_toast.jpg'
+        }
+      ],
+      nextRecipeId: 3,
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
-        <RecipeList />
+        <RecipeList onDelete={this.onDelete} recipes={this.state.recipes}/>
       </div>
     );
   }
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -4,37 +4,14 @@ import Recipe from './Recipe';
 import './RecipeList.css';
 
 class RecipeList extends Component {
-  static defaultProps = {
-    recipes: [
-      {
-        title:"Pasta",
-        instructions:'Mix ingredients',
-        ingredients: ['Flour', 'Water'],
-        img:'spaghetti.jpg'
-      },
-      {
-        title: "Milkshake",
-        instructions: 'Combine ice cream and milk. Blend until creamy',
-        ingredients: ['2 Scoops Ice cream', '8 ounces milk'],
-        img: 'milkshake.jpg'
-      },
-      {
-        title: 'Avocado Toast',
-        instructions: 'Toast bread. Slice avocado and spread on bread. Add salt, oil, and pepper to taste.',
-        ingredients: ['2 slices of bread', '1 avocado', '1 tablespoon olive oil', '1 pinch salt', 'pepper'],
-        img: 'avocado_toast.jpg'
-      }
-    ]
-  }
-
   static propTypes = {
     recipes: PropTypes.arrayOf(PropTypes.object).isRequired
   }
 
   render() {
-    const recipes = this.props.recipes.map((r, i) => (
+    const recipes = this.props.recipes.map((r, id) => (
       <Recipe
-        key={i}
+        key={id}
         {...r}
       />
     ))
